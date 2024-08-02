4ecec98c1e0e1a75916c776cbc023d83c8864e45
hung <tiviluson@gmail.com>
2024-08-02 03:09:20 -0400

    feat: Demo files (plug and play)

---
+++ test.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "http://127.0.0.1:8000/detect"
+headers = {
+    "accept": "application/json",
+    # "Content-Type": "multipart/form-data"
+}
+files = {
+    "file": open("sample_img.jpg", "rb")
+}
+
+response = requests.post(url, headers=headers, files=files)
+
+print(response.json())
